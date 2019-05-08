64f22dec5fd228bbad3f8b66b955364ffd56247e
PatouBC <pbergez@outlook.fr>
2019-05-08 15:25:07 +0200

    avec user

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/", name="accueilClass")
@@ -13,6 +14,14 @@ Class DefaultController extends AbstractController
      * @Route("/", name="index")
      */
     public function index()
+    {
+        return $this->render('default/index.html.twig');
+    }
+    /**
+     * @Route("/admin", name="indexAdmin")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function indexAdmin()
     {
         return $this->render('default/index.html.twig');
     }
